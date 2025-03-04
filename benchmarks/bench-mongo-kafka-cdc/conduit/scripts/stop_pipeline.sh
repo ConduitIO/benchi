3ea90214517a57cf06257eceb57ab757f4445f4e
Haris Osmanagic <haris@meroxa.io>
2025-03-04 13:48:25 +0100

    Mongo to Kafka CDC test

--- benchmarks/bench-mongo-kafka-cdc/conduit/scripts/stop_pipeline.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-# Save the current timestamp
-date +"%Y-%m-%d %H:%M:%S" > pipeline_stop_time.txt
-
-# Run the POST request
-curl -X POST localhost:8081/v1/pipelines/mongo-to-kafka/stop
