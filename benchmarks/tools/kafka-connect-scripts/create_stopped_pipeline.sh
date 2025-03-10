4bb593f08e88a68e3a96e2d99d8724f6eeb06ec3
Haris Osmanagic <haris@meroxa.io>
2025-03-10 10:47:11 +0100

    Add snapshot tests

--- benchmarks/tools/kafka-connect-scripts/create_stopped_pipeline.sh
+++ benchmarks/tools/kafka-connect-scripts/create_stopped_pipeline.sh
@@ -2,6 +2,6 @@
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-curl -s -X POST -H "Content-Type: application/json" -d @"$SCRIPT_DIR/connector.json" localhost:8083/connectors
+"$SCRIPT_DIR/create_started_pipeline.sh"
 
 "$SCRIPT_DIR/stop_pipeline.sh"
